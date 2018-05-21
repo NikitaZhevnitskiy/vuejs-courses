7dbf472373bbe6069a0b62f0ff45ae8df6fe6fd2
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-05-21 20:38:33 +0200

    update video list and item

--- video-browser/src/components/VideoList.vue
+++ video-browser/src/components/VideoList.vue
@@ -1,9 +1,10 @@
 <template>
-  <ul class="list-group">
+  <ul class="list-group col-md-4">
     <VideoListItem
         v-for="video in videos"
         :video="video"
         :key="video.etag"
+        @videoSelect="onVideoSelect"
     />
   </ul>
 </template>
@@ -18,7 +19,12 @@ export default {
   components: {
     VideoListItem
   },
-  props: ['videos']
+  props: ['videos'],
+  methods: {
+    onVideoSelect(video) {
+      this.$emit('videoSelect', video)
+    }
+  }
 };
 
 </script>
