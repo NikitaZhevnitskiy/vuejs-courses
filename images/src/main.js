e9a8d57c3715d5f491a5e19f0163e13ce79dc165
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-05-29 21:14:35 +0200

    add router

--- images/src/main.js
+++ images/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
 import store from './store';
+import AuthHadler from './components/AuthHadler'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  // replace #-router
+  mode: 'history',
+  routes: [
+    { path: '/oauth2/callback', component: AuthHadler }
+  ]
+});
 
 new Vue({
+  router,
   store,
   render: h => h(App)
 }).$mount('#app');
