d539fa4f4e1d630f01dad5ee8d516a8ae0fb057c
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-06-03 15:28:09 +0200

    upload img feature

--- images/src/components/UploadForm.vue
+++ images/src/components/UploadForm.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>
-    Upload form
+  <div class="dropper">
+    <input
+        type="file"
+        @change="uploadImages($event.target.files)"
+        multiple
+        accept="image/*"
+    />
+    <span>Drag files here!</span>
   </div>
 </template>
 
 <script>
-export default{
-  name: 'UploadForm'
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'UploadForm',
+  methods: mapActions(['uploadImages'])
 }
 </script>
+
+<style scoped>
+.dropper{
+  height: 30vh;
+  border: 2px dashed black;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+.dropper:hover {
+  background-color: #eee;
+}
+input {
+  width: 100%;
+  height: 30vh;
+  position: absolute;
+  opacity: 0;
+}
+</style>
