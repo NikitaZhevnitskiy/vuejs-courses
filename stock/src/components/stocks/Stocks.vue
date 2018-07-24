b42a9692374c0c3e2977db9ce978380126adbd6d
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2018-07-24 10:49:01 +0200

    add: stock boilerplate

--- stock/src/components/stocks/Stocks.vue
+++ stock/src/components/stocks/Stocks.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Stocks comp</h1>
+<div>
+  <app-stock v-for="stock in stocks"></app-stock>
+</div>
 </template>
+
+<script>
+  import Stock from './Stock'
+  export default {
+    data() {
+      return {
+        stocks: [
+          {id: 1, name: 'BMW', price: 110},
+          {id: 2, name: 'W', price: 1102},
+          {id: 3, name: 'B', price: 43},
+          {id: 4, name: 'M', price: 99},
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
